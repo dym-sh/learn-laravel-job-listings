71faf5a70d12bbc7eb3afe8d7ba6598db2f966f9
Dym Sohin <re@dym.sh>
2024-08-11 16:24:44 +0200

    /register

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+use App\Models\User;
+
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +18,18 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd('todo');
+        $validatedAttributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'], // password_confirmation
+        ]);
+
+        $user = User::create( $validatedAttributes );
+
+        Auth::login($user);
+
+        return redirect('/jobs');
+
     }
 }
