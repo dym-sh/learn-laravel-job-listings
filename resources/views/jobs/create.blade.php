079abb9ed55faa7220d4ffbc0a32c50372fa47bc
Dym Sohin <re@dym.sh>
2024-08-11 11:17:33 +0200

    form validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -15,23 +15,40 @@
         <div class="sm:col-span-4">
           <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
           <div class="mt-2">
-              <input type="text" name="title" id="title" autocomplete="title" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Web-Developer">
+              <input type="text" name="title" id="title" autocomplete="title" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Web-Developer" >
           </div>
+          @error('title')
+            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+          @enderror
         </div>
 
         <div class="sm:col-span-4">
           <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
           <div class="mt-2">
-              <input type="text" name="salary" id="salary" autocomplete="salary" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="40.000 € per year">
+              <input type="text" name="salary" id="salary" autocomplete="salary" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="40.000 € per year" >
           </div>
+          @error('salary')
+            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+          @enderror
         </div>
 
       </div>
+
+      {{-- <div class="mt-10">
+        @if($errors->any())
+          <ul>
+            @foreach($errors->all() as $error)
+              <li class="text-red-500 italic">{{ $error }}</li>
+            @endforeach
+          </ul>
+        @endif
+      </div> --}}
+
     </div>
   </div>
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+    <x-button href="/jobs" class="text-sm font-semibold leading-6 text-gray-100">Cancel</x-button>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
 </form>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/jobs', function () {
     ]);
 });
 Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
