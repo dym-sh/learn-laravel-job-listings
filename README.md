26af10195a7d03301d1017d79d67366488c23aec
Dym Sohin <re@dym.sh>
2024-08-11 13:42:12 +0200

    RESTful api

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 working along the tutorial of https://www.youtube.com/watch?v=SqTdHCTWqks
 
+## themes asserted
+
+- Object-Oriented PHP code
+- Laravel defaults
+- Eloquent, SQL
+- RESTful api
+
+
 final state should be something like https://github.com/JeffreyWay/30-days-to-learn-laravel/
--- routes/web.php
+++ routes/web.php
@@ -1,84 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+// Route::controller(JobController::class)->group( function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}',  'show');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::post('/jobs', 'store');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-// Create
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-Route::get('/jobs/{id}', function ( $id ) {
-
-    $job = Job::find($id);
-
-    return view('jobs.show', [ 'job' => $job ]);
-});
-
-// Edit
-Route::get('/jobs/{id}/edit', function ( $id ) {
-
-    $job = Job::find($id);
-
-    return view('jobs.edit', [ 'job' => $job ]);
-});
-
-// Update
-Route::patch('/jobs/{id}', function ( $id ) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    //auth;
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-    $job->save();
-
-    return redirect( '/jobs/'.$job->id );
-
-});
-
-// Destroy
-Route::delete('/jobs/{id}', function ( $id ) {
-    //auth;
-    Job::findOrFail($id)->delete();
-
-    return redirect( '/jobs/' );
-
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class, [
+    // 'only' => ['index', 'show', 'create', 'store']
+]);
